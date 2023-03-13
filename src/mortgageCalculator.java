e5c085208e155854c8aefb0f7b861ad32e5ed094
Raushan Kumar <raukum74@gmail.com>
2023-03-13 13:12:12 +0530

    Learnt How to take inputs in java

---
+++ src/mortgageCalculator.java
@@ -0,0 +1,20 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class mortgageCalculator {
+    public static void main(String[] args){
+        Scanner read=new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = read.nextInt();
+        System.out.print("Annual Interest Rate: ");
+        float rate = read.nextFloat();
+        System.out.print("Periods (Years):");
+        short time = read.nextShort();
+        rate/=1200;
+        time*=12;
+        double calc =Math.pow((1+rate),time);
+        double amount=principal*(rate*calc)/(calc-1);
+        NumberFormat mortgage=NumberFormat.getCurrencyInstance();
+        System.out.println(mortgage.format(amount));
+    }
+}
