7fd8fcfcf7992369ff9c3f3feb240ff0eaf2a09b
Raushan Kumar <raukum74@gmail.com>
2023-03-13 22:32:27 +0530

    Updated Calculator

--- src/mortgageCalculator.java
+++ src/mortgageCalculator.java
@@ -4,12 +4,27 @@ import java.util.Scanner;
 public class mortgageCalculator {
     public static void main(String[] args){
         Scanner read=new Scanner(System.in);
-        System.out.print("Principal: ");
+        System.out.print("Principal (1K-1M): ");
         int principal = read.nextInt();
+        while(principal<1_000 || principal>1_000_000){
+            System.out.println("Enter a number between 1,000 and 1,000,000");
+            System.out.print("Principal (1K-1M): ");
+            principal = read.nextInt();
+        }
         System.out.print("Annual Interest Rate: ");
         float rate = read.nextFloat();
+        while(rate<1 || rate>30){
+            System.out.println("Enter a number between 1 and 30");
+            System.out.print("Rate: ");
+            rate = read.nextFloat();
+        }
         System.out.print("Periods (Years):");
         short time = read.nextShort();
+        while(time<1 || time>30){
+            System.out.println("Enter a number between 1 and 30");
+            System.out.print("Time: ");
+            time = read.nextShort();
+        }
         rate/=12;
         rate/=100;
         time*=12;
